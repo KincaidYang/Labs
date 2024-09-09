79c13a41817d17957693a947c41b4586f556e2ce
KincaidYang <91786638+KincaidYang@users.noreply.github.com>
2024-09-09 12:42:23 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/KincaidYang/Labs/tree/main/blog',
+            'https://github.com/KincaidYang/Labs/tree/main/',
         },
         gtag: {
           trackingID: 'G-DHNVHPYK9P',
